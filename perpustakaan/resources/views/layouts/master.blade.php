2c6597e52dca15a3c93de33ace68d0c605d0c4e7
Hana Sa'dyah <sadyahhana19@gmail.com>
2022-10-08 20:55:16 +0700

    pindah pengarang ke menu buku

--- perpustakaan/resources/views/layouts/master.blade.php
+++ perpustakaan/resources/views/layouts/master.blade.php
@@ -47,17 +47,7 @@
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Data Buku</span></a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="{{route('pengarang.index')}}">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Data Pengarang</span></a>
-            </li>
+          
             <!-- Divider -->
             <hr class="sidebar-divider">
 
@@ -89,10 +79,11 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <a class="collapse-item" href="/buku/index">Buku</a>
                         <a class="collapse-item" href="/peminjaman/index">Peminjaman</a>
+                        <a class="collapse-item" href="/pengarang">Pengarang</a>
                     </div>
                 </div>
             </li>
-
+            
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
