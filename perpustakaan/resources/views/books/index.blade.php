a61b6a06727be4ff93d6e8090299944062bc1c3a
Fennugrh <anviewyosh@gmail.com>
2022-10-08 19:12:24 +0700

    testing push

---
+++ perpustakaan/resources/views/books/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.master')
+
+@section('title')
+    Halaman Buku Perpustakaan
+@endsection
+
+@section('content')
+<div class="row mt-3">
+  <div class="col">
+    <div class="card">
+    <img src="#" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">PUMA</h5>
+      <p class="card-text">Essentials Kids' Cap</p>
+      <p class="card-text text-danger">Rp 159.000</p>
+      <a href="#" class="btn btn-primary btn-block"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+    </div>
+  </div>
+  </div>
+  <div class="col">
+    <div class="card">
+    <img src="#" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">H&M</h5>
+      <p class="card-text">Regular Fit Hoodie</p>
+      <p class="card-text text-danger">Rp 599.900</p>
+      <a href="#" class="btn btn-primary btn-block"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+    </div>
+  </div>
+  </div>
+  <div class="col">
+    <div class="card">
+    <img src="#" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">H&M</h5>
+      <p class="card-text">Regular Fit Hoodie</p>
+      <p class="card-text text-danger">Rp 599.900</p>
+      <a href="#" class="btn btn-primary btn-block"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+    </div>
+  </div>
+  </div> <div class="col">
+    <div class="card">
+    <img src="#" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">H&M</h5>
+      <p class="card-text">Regular Fit Hoodie</p>
+      <p class="card-text text-danger">Rp 599.900</p>
+      <a href="#" class="btn btn-primary btn-block"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
+    </div>
+  </div>
+  </div>
+</div>
+@endsection 
+
