faefda0698da8aa0a95912403b509e2c602d4f36
Hana Sa'dyah <sadyahhana19@gmail.com>
2022-10-08 20:52:09 +0700

    revisi route web

--- perpustakaan/routes/web.php
+++ perpustakaan/routes/web.php
@@ -13,3 +13,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 //route pengarang
 Route::get('/pengarang', [PengarangController::class, 'index'])->name('pengarang.index');
+
+Route::get('/buku/index', [App\Http\Controllers\BukuController::class, 'index'])->name('buku.index');
+
+Route::get('/peminjaman/index', [App\Http\Controllers\PeminjamanController::class, 'index'])->name('Peminjaman.index');
